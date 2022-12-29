924ca0e33b7e07a7a1a2bdba903140d133b7d380
leejy811 <lee38956162@gmail.com>
2022-12-30 02:01:20 +0900

    Test 2

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Vector3 position;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
