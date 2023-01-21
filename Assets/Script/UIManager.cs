143b734f1be94e22b626d0f7a8992fb5a621fa85
seul <angel3030303@gmail.com>
2023-01-21 18:58:42 +0900

    UI 피드백 수정
    폰트 및 메달 트로피 아이콘 수정

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public Player player;
+
+    public Text scoreText;
 
     void Update()
     {
