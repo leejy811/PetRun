ecec2c18a50fddeaf59870c8434dd605ceae9575
leejy811 <lee38956162@gmail.com>
2023-02-08 05:06:23 +0900

    UI 해상도 보정 모든 기기에 적용 가능하도록 수정

--- Assets/Script/UISetting.cs
+++ Assets/Script/UISetting.cs
@@ -8,21 +8,26 @@ public class UISetting : MonoBehaviour
     void Awake()
     {
         RectTransform rectTransform = GetComponent<RectTransform>();
-        
-        if (Screen.width > 1920)
+
+        if (Screen.width / 16 == Screen.height / 9)
+            return;
+
+        int Ratio = Mathf.Min(Screen.width / 16, Screen.height / 9);
+
+        if (Screen.width > Ratio * 16)
         {
             if (rectTransform.pivot.x == 0)
-                rectTransform.anchoredPosition += new Vector2((Screen.width - 1920) / 2, 0);
+                rectTransform.anchoredPosition += new Vector2((Screen.width - Ratio * 16) / 2, 0);
             else if (rectTransform.pivot.x == 1)
-                rectTransform.anchoredPosition += new Vector2((Screen.width - 1920) / -2, 0);
+                rectTransform.anchoredPosition += new Vector2((Screen.width - Ratio * 16) / -2, 0);
         }
 
-        if (Screen.height > 1080)
+        if (Screen.height > Ratio * 9)
         {
             if (rectTransform.pivot.y == 0)
-                rectTransform.anchoredPosition += new Vector2(0, (Screen.height - 1080) / 2);
+                rectTransform.anchoredPosition += new Vector2(0, (Screen.height - Ratio * 9) / 2);
             else if (rectTransform.pivot.y == 1)
-                rectTransform.anchoredPosition += new Vector2(0, (Screen.height - 1080) / -2);
+                rectTransform.anchoredPosition += new Vector2(0, (Screen.height - Ratio * 9) / -2);
         }
 
     }
